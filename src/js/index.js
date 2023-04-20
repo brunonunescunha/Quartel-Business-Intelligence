d2654fecffd3dcadf8649a603186d7c31e1fafac
Bruno Nunes Cunha <brunonunes2505@gmail.com>
2023-04-20 11:45:11 -0300

    Criação do site do Marlon foi feito o menu de navagaçãode site e inicio

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const menus = document.querySelectorAll(".menu")
+
+menus.forEach((menu) =>{
+    menu.addEventListener("click", () => {
+        const menuAtual = document.querySelector(".ativo")
+
+        if(menuAtual){
+            menuAtual.classList.remove("ativo")
+        }
+        menu.classList.add("ativo")
+    })
+
+    
+})
+
+const removeAtivoDoMenu = document.querySelector(".inicio") 
+console.log(removeAtivoDoMenu);
+removeAtivoDoMenu.addEventListener("mouseenter", () => {
+
+    const menuativo = document.querySelector(".menu-servicos")
+    menuativo.classList.remove("ativo")
+})
